50b5dff499426cf089f952c318423534e38e8865
Tonje <tonje_martine_@hotmail.com>
2020-10-20 15:27:43 +0200

    Kjørte en test i main, sjekket kode som var oppitt i oppgaven (denne skulle være feilfri, ikke kaste noen unntak). Ingen unntak er kastet, og foreløpig virker det som denne koden er grei!

--- src/no/oslomet/cs/algdat/Eksamen/TestMain.java
+++ src/no/oslomet/cs/algdat/Eksamen/TestMain.java
@@ -5,7 +5,15 @@ import java.util.Comparator;
 public class TestMain {
     public static void main(String[] args) {
 
-        EksamenSBinTre<String> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
+        //EksamenSBinTre<String> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
+        //System.out.println(tre.antall());
+
+        Integer[] a = {4, 7, 2, 9, 5, 10, 8, 1, 3, 6};
+        EksamenSBinTre<Integer> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
+
+        for(int verdi : a) {
+            tre.leggInn(verdi);
+        }
         System.out.println(tre.antall());
 
     }
